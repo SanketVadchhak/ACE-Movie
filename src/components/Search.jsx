4779f6fec1be4519df37865a15acb825e235797d
Sanket vadchhak <128171370+SanketVadchhak@users.noreply.github.com>
2025-09-19 19:51:12 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '/search.svg';
 
 const Search = ({search, setSearch}) => {
   return (
     <div className='search'>
         <div className=''>
-            <img src="/search.svg" alt="search" />
+            <img src={logo} alt="search" />
             <input type="text" placeholder='Search through all movies' value={search} onChange={(e) => setSearch(e.target.value)}/>
         </div>
     </div>
